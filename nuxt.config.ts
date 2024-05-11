65ab3bc1c2c46b132fb6ee71324914bb52bd2719
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-11 19:30:46 -0400

    feat(config): add custom fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,13 @@ Ya sea que desees desarrollar aplicaciones de sistemas, videojuegos, criptomoned
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1'
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: "preconnect", href:"https://fonts.googleapis.com" },
+        { rel: "preconnect", href:"https://fonts.gstatic.com", crossorigin: true },
+        { rel: 'stylesheet', href:"https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" },
+        { rel: 'stylesheet', href:"https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" },
+      ]
     }
   },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    fontFamily: {
+      "alfa-slab": ["Alfa Slab One", "sans-serif"],
+      "fira-sans": ["Fira Sans", "sans-serif"],
+      "work-sans": ["Work Sans", "sans-serif"],
+    },
+    extend: {
+      fontSize: {
+        'half': '3.5rem',
+      },
+      lineHeight: {
+        '3lh': '2.8rem',
+      },
+      backgroundImage: (theme) => ({
+        "anuncios": "url('https://i.imgur.com/tDlT9sr.jpg')",
+        "kaku-dev": "url('https://www.rustlang-es.org/kaku.avif')",
+        "kaku": "url('https://www.rustlang-es.org/kaku.avif')",
+      }),
+    },
+  },
+};
