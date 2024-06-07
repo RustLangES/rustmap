d7823f6f3a34248339f34f19a9a1715242b9f582
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-06-07 01:37:10 -0400

    ci: remove ssr and add url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   modules: [ "@nuxt/content", "@nuxtjs/tailwindcss", "@nuxtjs/seo" ],
   site: {
+    url: 'https://roadmap.rustlang-es.org',
     name: 'Hoja de Ruta Definitiva para Aprender Rust: Desde Principiante hasta Experto',
     description: `
 ¿Estás listo para dominar uno de los lenguajes de programación más potentes y eficientes? Nuestra hoja de ruta te guiará paso a paso en tu viaje de aprendizaje de Rust, desde los conceptos básicos hasta las técnicas avanzadas. Diseñada para principiantes y desarrolladores experimentados, esta guía exhaustiva te ayudará a construir una sólida base en Rust y a aprovechar al máximo su rendimiento, seguridad y concurrencia.
