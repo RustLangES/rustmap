19c6b92f83452a134906136f9be032a6c9adcd9a
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-06-23 22:01:12 -0400

    feat(ui): header bar on sidebar

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -4,10 +4,14 @@
   <Roadmap />
   <div v-if="content" :class="['fixed top-0 h-screen', sidebarClass]">
     <div
-        class="top-0 flex h-full w-full overflow-y-auto flex-col items-center bg-white p-4 focus:outline-0 sm:p-6"
+        class="top-0 flex h-full w-full flex-col items-center bg-white p-4 focus:outline-0 sm:p-6"
     >
+      <div class="w-full flex flex-row justify-between px-4 pb-2">
+        <span>[TODO] Status</span>
+        <span class="hover:cursor-pointer" @click="closeSidebar">X</span>
+      </div>
       <ContentRenderer :key="content._id" :value="content">
-        <ContentRendererMarkdown class="flex flex-col gap-3 text-black w-full m:max-w-[800px] sm:max-w-[600px]" :value="content" />
+        <ContentRendererMarkdown class="flex flex-col gap-3 text-black overscroll-x-none overflow-y-auto w-full m:max-w-[800px] sm:max-w-[600px]" :value="content" />
         <div class="flex flex-col items-start w-full m:max-w-[800px] sm:max-w-[600px]">
           <h4 id="related-content" class="text-black mb-3">
             <a href="#related-content">Contenido Extra Relacionado</a>
@@ -46,5 +50,6 @@ onMounted(async () => {
   showSidebar.value = contentResult && (route.query.fromClick || false)
 })
 
+const closeSidebar = () => content.value = null;
 const sidebarClass = computed(() => showSidebar ? 'right-0' : '')
 </script>
