4e7d830ca156cc880ad61deb590b80f2d4dd304f
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-06-25 17:28:20 -0400

    fix(ui): dark and light mode fixes

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,33 +5,33 @@
   <Teleport to="body">
   <div v-if="content" :class="['fixed top-0 right-0 h-screen', sidebarClass]">
     <div
-        class="flex overflow-y-auto overflow-x-none h-full w-full flex-col items-center bg-white p-4 focus:outline-0 sm:p-6"
+        class="prose dark:prose-invert flex overflow-y-auto overflow-x-hidden h-full w-full flex-col items-center p-4 focus:outline-0 sm:p-6 bg-orange-50 dark:bg-gray-800"
     >
-      <div class="w-full flex flex-row justify-between text-black px-4 pb-2">
+      <div class="w-full flex flex-row justify-between px-4 pb-2">
         <Dropdown ref="statusDropDown" :border="false">
           <!-- trigger element -->
           <template #trigger>
-            <button type="button">{{status}}</button>
+            <button type="button" :class="['px-4 py-2 border-black border', status.toLowerCase()]">{{status}}</button>
           </template>
 
           <!-- contents display in dropdown -->
-          <div class="rounded flex flex-col text-black">
-            <div
+          <ul class="flex flex-col bg-orange-100 dark:bg-gray-600">
+            <li
               v-for="(s, i) in allStatus"
               :key="'status-' + i"
               @click="changeStatus(s)"
-              class="px-4 py-2 hover:cursor-pointer border-b border-black"
+              :class="['px-4 py-2 hover:cursor-pointer border-b border-black hover:bg-orange-50 dark:hover:bg-gray-500', s.toLowerCase()]"
               v-text="s"
             >
-            </div>
-          </div>
+            </li>
+          </ul>
         </Dropdown>
         <span class="hover:cursor-pointer" @click="closeSidebar">X</span>
       </div>
       <ContentRenderer :key="content._id" :value="content">
-        <ContentRendererMarkdown class="prose flex flex-col w-full m:max-w-[800px] sm:max-w-[600px]" tag="article" :value="content" />
+        <ContentRendererMarkdown class="flex flex-col w-full m:max-w-[800px] sm:max-w-[600px]" tag="article" :value="content" />
         <div class="flex flex-col items-start w-full m:max-w-[800px] sm:max-w-[600px]">
-          <h4 id="related-content" class="text-black mb-3">
+          <h4 id="related-content" class="mb-3">
             <a href="#related-content">Contenido Extra Relacionado</a>
           </h4>
           <a
@@ -91,3 +91,18 @@ function changeStatus(val) {
   statusDropDown.value.close()
 }
 </script>
+
+<style>
+.pendiente::before,
+.leyendo::before,
+.completado::before,
+.omitir::before {
+  content: '•';
+  margin-right: 5px;
+}
+
+.pendiente::before { color: var(--tw-text-gray-950); }
+.leyendo::before { color: var(--tw-text-orange-500); }
+.completado::before { color: var(--tw-text-green-600); }
+.omitir::before { color: var(--tw-text-slate-500); }
+</style>
