9dd86e91c91aba0092edd96f58abff37fd64a5a5
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-07-07 13:10:23 -0400

    feat(preview): basic cli arguments system

--- crates/preview/src/main.rs
+++ crates/preview/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args().skip(1);
+
+    let _out = args
+        .next()
+        .expect("The first argument must be the output directory");
+
+    let files = if args.len() > 1 {
+        args.collect::<Vec<String>>()
+    } else {
+        let dir = args.next().expect(
+            "The second argument must be the directory with the content or files to be processed",
+        );
+
+        // get all files recursively on dir
+        walkdir::WalkDir::new(&dir)
+            .sort_by_file_name()
+            .into_iter()
+            .flat_map(|d| d.map(|d| d.path().to_str().unwrap().to_string()).ok())
+            .collect()
+    };
 }
